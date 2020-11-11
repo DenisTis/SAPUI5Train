f8e83f0690b2b0d638a00533aef05de128e6521c
Denis Tislenko <denis.tislenko@readsoft.com>
2020-11-11 13:38:39 +0100

    northwind ready

--- README.md
+++ README.md
@@ -19,3 +19,14 @@ server request.
 
 Optionally: target in connectionJson should point to the same location as
 datasource uri in manifest.json
+
+## Important Fiori links:
+
+- [Design guidelines](https://experience.sap.com/fiori-design-web/). Contains
+  general rules. Also allows to select, which element is used in which case.For
+  example, search for element "Switch" and read "Usage" section.
+
+- Tutorials for Fiori versions. Contains as well "API Reference" and "Samples".
+
+  - [1.38.7](https://sapui5.hana.ondemand.com/1.38.7/#docs/guide/167193ced54c41c3961d7df3479d7bbe.html)
+  - [1.71.6](https://sapui5.hana.ondemand.com/1.71.6/#/topic/8b49fc198bf04b2d9800fc37fecbb218)
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,21 +4,22 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("companyRepo.appName.controller.App", {
-      onInit: function() {
-        var me = this;
-        me.getView().setBusy(true);
-        var oModel = new sap.ui.model.odata.v2.ODataModel({
-          serviceUrl: "/Northwind/Northwind.svc",
-          //          serviceUrl: "/sap/opu/odata/backendServerPath",
-        });
-        oModel.metadataLoaded().then(function(oEvent) {
-          console.log(oEvent);
-          //sap default model has no name!
-          me.getView().setModel(oModel);
-          //me.getView().setModel(oModel, "northwind");
-          me.getView().setBusy(false);
-        });
-      },
+      //Old way of metadata initialization. Currently: define backend service in two sections of manifest.json file
+      // onInit: function() {
+      // var me = this;
+      // me.getView().setBusy(true);
+      // var oModel = new sap.ui.model.odata.v2.ODataModel({
+      //   serviceUrl: "/Northwind/Northwind.svc",
+      //   //          serviceUrl: "/sap/opu/odata/backendServerPath",
+      // });
+      // oModel.metadataLoaded().then(function(oEvent) {
+      //   console.log(oEvent);
+      //   //sap default model has no name!
+      //   me.getView().setModel(oModel);
+      //   //me.getView().setModel(oModel, "northwind");
+      //   me.getView().setBusy(false);
+      // });
+      // },
     });
   }
 );
